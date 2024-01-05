0a63bc7d36869ce028e23bf65cda45b81c6a2dea
Mauricio <mauricio.ariel.buda@gmail.com>
2024-01-05 17:07:15 -0300

    first commit

---
+++ js/firestoreConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABZOA8qx3lXr7Z2POXoPaT-pToiXvVEfc",
+  authDomain: "notas-307ba.firebaseapp.com",
+  projectId: "notas-307ba",
+  storageBucket: "notas-307ba.appspot.com",
+  messagingSenderId: "558524290384",
+  appId: "1:558524290384:web:7cba0a20d83c3a4cdf8855"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
