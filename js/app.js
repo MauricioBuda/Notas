36ce694c906a8c0ebedf8dded6a96554ca900d8f
Mauricio <mauricio.ariel.buda@gmail.com>
2024-03-19 14:56:04 -0300

    css btn bell

--- js/app.js
+++ js/app.js
@@ -119,7 +119,7 @@ let canceladasCards = document.getElementById("canceladas-cards");
 // Variables de la ventana modal, de cards grandes ↓
 let modalCard = document.getElementById("cardEnModal");
 let modalFooter = document.getElementById("modal_footerID");
-let svgBell = `<svg class="img-campana-card" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+let svgBell = `<svg class="img-campana-card" xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
 </svg>`;
 
@@ -1230,7 +1230,7 @@ function agregarCardAlContenedor(tarea) {
             <p>URGENCIA: <br> ${tarea.urgencia}</p>
             <p>CREACIÓN: <br> ${tarea.fechaCreacion}</p>
             <p>SECCIÓN: <br> ${tarea.seccion}</p>
-            <button class="button-campana-card">${svgBell}</button>
+
             <button id="${botonFinalizarID}" class="btn botonesCards" >Finalizar</button>
             <button id="${botonMasOpcionesID}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn botonesCards" >Opciones</button>
           </div>
@@ -1244,7 +1244,6 @@ function agregarCardAlContenedor(tarea) {
           <p>URGENCIA: <br> ${tarea.urgencia}</p>
           <p>CREACIÓN: <br> ${tarea.fechaCreacion}</p>
           <p>SECCIÓN: <br> ${tarea.seccion}</p>
-          <button class="button-campana-card">${svgBell}</button>
           <button id="${botonFinalizarID}" class="btn botonesCards" >Finalizar</button>
           <button id="${botonMasOpcionesID}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn botonesCards" >Opciones</button>
         </div>
@@ -1258,7 +1257,6 @@ function agregarCardAlContenedor(tarea) {
           <p>URGENCIA: <br> ${tarea.urgencia}</p>
           <p>CREACIÓN: <br> ${tarea.fechaCreacion}</p>
           <p>SECCIÓN: <br> ${tarea.seccion}</p>
-          <button class="button-campana-card">${svgBell}</button>
           <button id="${botonFinalizarID}" class="btn botonesCards" >Finalizar</button>
           <button id="${botonMasOpcionesID}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn botonesCards" >Opciones</button>
         </div>
